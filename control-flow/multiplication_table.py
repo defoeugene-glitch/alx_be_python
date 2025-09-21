e9f1c49bab28f5ecccfbf87ee44f498559c93278
Eugene Nana Darko Appiah <defoeugene.04@gmail.com>
2025-09-21 18:04:36 +0000

    Fixed the sign

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 
 for item in range(1,11):
-     print(f"{number} x {item} = {number * item}")
+     print(f"{number} * {item} = {number * item}")
